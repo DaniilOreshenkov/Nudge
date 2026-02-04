a96d4a6898add26e5abfb1258127112af7c84531
Danya <d@MacBook-Pro-Danya.local>
2026-02-05 00:07:52 +1000

    Initial commit

---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Danya on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
